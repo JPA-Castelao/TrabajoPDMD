6fdfaef5df5ce2adaa63a578b43d3ac22fa7bba6
dam <mbelosoleon@danielcastelao.org>
2025-11-11 12:47:59 +0100

    Funciones numeroRandom y actualizarRandom

--- app/src/main/java/com/dam/trabajopmdm/MiViewModel.kt
+++ app/src/main/java/com/dam/trabajopmdm/MiViewModel.kt
@@ -1,8 +1,10 @@
 package com.dam.trabajopmdm
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dam.mvvm_basic.Datos
 import com.dam.mvvm_basic.Estados
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -16,7 +18,22 @@ class MiViewModel(): ViewModel() {
     //variable puntuacion
     val puntuacion = MutableStateFlow<Int?>(0)
 
+    //Función para crear un número random
+    fun numeroRandom(){
+        estadoActual.value = Estados.GENERANDO
+        Log.d("ViewModel","Estado generando")
+        _numbers.value = (0..3).random()
+        Log.d("ViewModel","Número random generado: $_numbers")
+        actualizarNumero(_numbers.value)
+    }
 
+    //Funcion para actualizar el número random
+    fun actualizarNumero(numero: Int){
+        Log.d("ViewModel","Actualizando el numero de la clase Datos")
+        Datos.numero = numero
+        estadoActual.value = Estados.ADIVINANDO
+        Log.d("ViewModel", "Estado adivinando")
+    }
 
 
 }
