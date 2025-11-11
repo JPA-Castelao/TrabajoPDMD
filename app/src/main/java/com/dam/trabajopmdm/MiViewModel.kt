184283756a6466de0a33e92ededf44dcd9bfe3ef
dam <mbelosoleon@danielcastelao.org>
2025-11-11 12:33:25 +0100

    variable _numbers

--- app/src/main/java/com/dam/trabajopmdm/MiViewModel.kt
+++ app/src/main/java/com/dam/trabajopmdm/MiViewModel.kt
@@ -1,8 +1,19 @@
 package com.dam.trabajopmdm
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dam.mvvm_basic.Estados
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class MiViewModel(): ViewModel() {
     //etiqueta para logcat
     private val TAG_LOG = "midebug"
+
+    //estados del juego
+    val estadoActual: MutableStateFlow<Estados> = MutableStateFlow(Estados.INICIO)
+
+    var _numbers = mutableStateOf(0)
+
+
 }
